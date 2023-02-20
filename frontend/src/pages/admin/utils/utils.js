7627d6b5c4f403097d64ce3e2a3106ec4f3ea1c5
Aryan Barsaiyan <82722871+AryanBarsaiyan@users.noreply.github.com>
2023-02-20 19:55:10 +0530

    cloud

--- frontend/src/pages/admin/utils/utils.js
+++ frontend/src/pages/admin/utils/utils.js
@@ -10,12 +10,12 @@ export const uploadImagesApiRequest = async (images, productId) => {
 };
 
 export const uploadImagesCloudinaryApiRequest = (images,productId) => {
-    const url = "https://api.cloudinary.com/v1_1/ddkj8wsjy/image/upload";
+    const url = "https://api.cloudinary.com/v1_1/jkhbnc-366gs/image/upload";
     const formData = new FormData();
     for (let i = 0; i < images.length; i++) {
         let file = images[i];
         formData.append("file", file);
-        formData.append("upload_preset", "x7ndidke");
+        formData.append("upload_preset", "acsijtdn");
         fetch(url, {
             method: "POST",
             body: formData,
